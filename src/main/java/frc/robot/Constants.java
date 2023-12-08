a01660c3bd51de706fb29f62bbd1cb462645720a
garethTatum <gtatum9@comcast.net>
2023-12-07 16:20:36 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class LEDConstants {
+    public static final Color8Bit kYellow = new Color8Bit(255, 150, 0);
+    public static final Color8Bit kPurple = new Color8Bit(255, 0, 255);
+    public static final Color8Bit kInitialMaroon = new Color8Bit(144, 56, 32);
+    public static final int kLightPort = 0; // <== Placeholder!!
+    public static final int kNumberOfLights = 29;
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/LEDControllerSubsystem.java
@@ -0,0 +1,105 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.awt.Color;
+import frc.robot.Constants.LEDConstants;
+
+public class LEDControllerSubsystem extends SubsystemBase {
+  /** Creates a new LEDSubsystem. */
+
+  protected Color8Bit currentColor;
+  protected AddressableLED addressableLED;
+  protected AddressableLEDBuffer addressableLEDBuffer;
+  protected boolean increasing = true;
+  protected boolean breathing;
+  protected boolean ledColorState = true;
+
+  public LEDControllerSubsystem() {
+    addressableLED = new AddressableLED(LEDConstants.kLightPort);
+    addressableLEDBuffer = new AddressableLEDBuffer(LEDConstants.kNumberOfLights);
+    addressableLED.setLength(addressableLEDBuffer.getLength());
+
+    addressableLED.setData(addressableLEDBuffer);
+    addressableLED.start();
+  }
+
+  public Command setColor(Color8Bit color) {
+    currentColor = color;
+    return runOnce(() -> {
+      for (int i = 0; i < addressableLEDBuffer.getLength(); i++) {
+        addressableLEDBuffer.setLED(i, color);
+      }
+    });
+  }
+
+  public Command startBreathing() {
+    return runOnce(() -> breathing = true);
+  }
+
+  public Command stopBreathing() {
+    return runOnce(() -> breathing = false);
+  }
+
+  public Command yellowLEDCommand() {
+    return stopBreathing().andThen(setColor(LEDConstants.kYellow));
+  }
+
+  public Command purpleLEDCommand() {
+    return stopBreathing().andThen(setColor(LEDConstants.kPurple));
+  }
+
+  public Command maroonLEDCommand() {
+    return startBreathing().andThen(setColor(LEDConstants.kInitialMaroon));
+  }
+
+  public Command toggleLEDCommand() {
+    return runOnce(() -> {
+      if (ledColorState) {
+        ledColorState = false;
+        stopBreathing().andThen(setColor(LEDConstants.kPurple)).schedule();;
+      }
+      else {
+        ledColorState = true;
+        stopBreathing().andThen(setColor(LEDConstants.kYellow)).schedule();;
+      }
+    });
+  }
+
+  public Color8Bit breathingColor() {
+    float[] HSBArray = Color.RGBtoHSB(currentColor.red, currentColor.green, currentColor.blue, null);
+    if (increasing) {
+        HSBArray[2] += 1.0;
+    } else {
+        HSBArray[2] -= 1.0;
+    }
+    if (HSBArray[2] > 80.0 && increasing == true) {
+        increasing = false;
+    } else if (HSBArray[2] < 20.0 && increasing == false) {
+        increasing = true;
+    }
+    int RGBOneDigit = Color.HSBtoRGB(HSBArray[0], HSBArray[1], HSBArray[2]);
+    Color tempColor = new Color(RGBOneDigit);
+    return new Color8Bit(tempColor.getRed(), tempColor.getGreen(), tempColor.getBlue());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("COLOR TOGGLESTATE", ledColorState);
+    if (breathing) {
+      currentColor = breathingColor();
+      setColor(currentColor);
+    }
+    // addressableLED.start();
+    addressableLED.setData(addressableLEDBuffer);
+  }
+}
+
